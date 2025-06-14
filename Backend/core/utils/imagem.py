e6fd928735b49e816d9365cd706f51f6afb41b03
LucasMaciel7 <lucasmacielcampos27@gmail.com>
2025-06-14 16:47:57 -0300

    reafctor: function for validate base 64

--- Backend/core/utils/imagem.py
+++ Backend/core/utils/imagem.py
@@ -2,6 +2,9 @@ import base64
 import os
 from uuid import uuid4
 from django.conf import settings
+from rest_framework import serializers
+
+
 
 def salvar_imagem_base64_em_arquivo(base64_str):
     pasta = 'imagem/alunos'  # ✅ Caminho desejado
@@ -20,3 +23,22 @@ def salvar_imagem_base64_em_arquivo(base64_str):
         f.write(base64.b64decode(data))
 
     return caminho_absoluto  # ✅ continue retornando absoluto como antes
+
+
+
+
+class Base64ImageValidatorMixin:
+    def validate_image_base64(self, value):
+        if not value.startswith("data:image"):
+            raise serializers.ValidationError("Imagem base64 inválida ou mal formatada.")
+
+        if ';base64,' not in value:
+            raise serializers.ValidationError("Formato base64 esperado não encontrado.")
+
+        try:
+            base64_part = value.split(';base64,')[1]
+            base64.b64decode(base64_part, validate=True)
+        except Exception:
+            raise serializers.ValidationError("Conteúdo base64 inválido.")
+
+        return value
