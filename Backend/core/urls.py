749739ff444f76b0fb63158794d9e9742b9e65d3
LucasMaciel7 <lucasmacielcampos27@gmail.com>
2025-06-14 16:48:19 -0300

    feat: implemented route prenseca

--- Backend/core/urls.py
+++ Backend/core/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from core.Controller.aluno import AlunoManagerRoute
 
+from core.Controller.presenca import PresencaManagerRoute
+
 urlpatterns = [
     path('alunos/', AlunoManagerRoute.as_view(), name='criar-aluno'),
+    path('registra_ponto/', PresencaManagerRoute.as_view(), name='registrar-presenca'),
 ]
