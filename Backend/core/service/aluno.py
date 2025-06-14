533cdb31c7b3f322765bfd96480093edf6bf5d4b
LucasMaciel7 <lucasmacielcampos27@gmail.com>
2025-06-14 16:46:56 -0300

    feat: compare students

--- Backend/core/service/aluno.py
+++ Backend/core/service/aluno.py
@@ -1,5 +1,8 @@
+import json
+import numpy as np
+import base64
+from io import BytesIO
 from core.repository.aluno import AlunoRepository
-
 import face_recognition
 
 class AlunoService:
@@ -37,7 +40,48 @@ class AlunoService:
 
         except Exception as e:
             raise ValueError(f"Erro ao processar a imagem: {str(e)}")
+
+
+
+    def compara_alunos(self, ra, base64_image):
+        """
+        Compara a imagem recebida com o aluno pelo RA.
+        """
         
+        # Busca o aluno pelo RA no banco de dados
+        aluno = self.aluno_repository.get_aluno_by_ra(ra)
+        if not aluno:
+            raise ValueError("Aluno não encontrado.")
+
+        # Extrai o encoding salvo do banco
+        try:
+            
+            aluno_encoding = aluno.embeddings
+        except Exception:
+            raise ValueError("Erro ao carregar encoding do aluno.")
+
+        try:
+            # Remove prefixo base64 se necessário
+            if ',' in base64_image:
+                base64_image = base64_image.split(',')[1]
+
+            # Decodifica e processa a imagem recebida
+            image_data = base64.b64decode(base64_image)
+            imagem_aluno = face_recognition.load_image_file(BytesIO(image_data))
+            encoding_list = face_recognition.face_encodings(imagem_aluno)
+
+            if not encoding_list:
+                raise ValueError("Nenhum rosto detectado na imagem enviada.")
+
+            encoding_aluno = encoding_list[0]
+
+            # Compara os embeddings
+            resultado_comparacao = face_recognition.compare_faces([aluno_encoding], encoding_aluno, tolerance=0.6)
+
+            return resultado_comparacao[0]  # True ou False
+
+        except Exception as e:
+            raise ValueError(f"Erro ao processar a imagem: {str(e)}")
         
         
             
