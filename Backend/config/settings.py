c2d3b699c7dfe9338ab67d21b2023134686fe069
LucasMaciel7 <lucasmacielcampos27@gmail.com>
2025-06-15 19:46:34 -0300

    feat: adotado timezone de são paulo

--- Backend/config/settings.py
+++ Backend/config/settings.py
@@ -113,12 +113,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+LANGUAGE_CODE = 'pt-br'
+TIME_ZONE = 'America/Sao_Paulo'
 USE_I18N = True
-
+USE_L10N = True
 USE_TZ = True
 
 
